73418fb547129af73f1f8ccd2a75b53c9cab43e9
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-04-01 20:25:01 +0200

    logoRef click() x 3

--- app/components/ui/Logo.tsx
+++ app/components/ui/Logo.tsx
@@ -22,10 +22,29 @@ export default function Logo() {
       if (windowWidth > 0) {
         if (ref !== undefined || ref !== null) {
           ref.current?.click();
+
           console.log("clicked");
         }
       }
     }, 2000);
+    setTimeout(() => {
+      if (windowWidth > 0) {
+        if (ref !== undefined || ref !== null) {
+          ref.current?.click();
+
+          console.log("clicked");
+        }
+      }
+    }, 3000);
+    setTimeout(() => {
+      if (windowWidth > 0) {
+        if (ref !== undefined || ref !== null) {
+          ref.current?.click();
+
+          console.log("clicked");
+        }
+      }
+    }, 5000);
     // clickRef();
 
     // return () => {
