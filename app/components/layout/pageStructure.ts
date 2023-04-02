977f4bed345955c3c8d8a5561a4e9df1cdd8bc34
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-04-02 23:14:06 +0200

    up down up 6

--- app/components/layout/pageStructure.ts
+++ app/components/layout/pageStructure.ts
@@ -1,6 +1,6 @@
 export const pageStructure = [
   { id: 1, page: "home", link: "#heroSection" },
-  { id: 2, page: "projects", link: "/" },
+  { id: 2, page: "projects", link: "/#projectsSection" },
   { id: 3, page: "about", link: "#techSection" },
   { id: 4, page: "contact", link: "#contactSection" },
 ];
