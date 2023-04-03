06e20845848e7691742298ca7e596c24475cca78
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-04-04 01:58:37 +0200

    siste før copy

--- app/ProjectsComponent.tsx
+++ app/ProjectsComponent.tsx
@@ -6,7 +6,7 @@ export default function ProjectsComponent() {
   return (
     <section
       id="projectsSection"
-      className="pb-32 bg-primary z-50 shadow-bsOne"
+      className="pb-16 bg-primary z-50 shadow-bsOne"
     >
       <div className="max-w-screen-lg md:mx-auto">
         <div className=" px-2 sm:px-4 md:mx-sidebarWidth">
